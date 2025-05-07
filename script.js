700665e90becf9dc492c91dd27c8da121775d7e3
SPYBILL <walissondaraissa@gmail.com>
2025-05-07 00:04:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+fetch('plataformas.json')
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('plataformas');
+    data.forEach(item => {
+      const card = document.createElement('div');
+      card.className = 'card';
+      card.innerHTML = `
+        <img src="${item.icone}" alt="${item.nome}">
+        <h2>${item.nome}</h2>
+        <p>${item.descricao}</p>
+        <a href="${item.link}" target="_blank">Acessar</a>
+      `;
+      container.appendChild(card);
+    });
+  });
 
